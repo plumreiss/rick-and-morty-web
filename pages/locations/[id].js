f2419a0cccc5953b3b048ab4f1f9ece2103e72f5
Angel Padrino <plumreiss@gmail.com>
2021-05-17 19:26:33 -0400

    ADD: rendering props to every location dinamic paths and residents

--- pages/locations/[id].js
+++ pages/locations/[id].js
@@ -1,7 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const API = "https://rickandmortyapi.com/api";
 
 export default function Location({ location, residents }) {
-  return <div>location</div>;
+  const { name, type, dimension } = location;
+
+  return (
+    <div>
+      <div>
+        <h2>{name}</h2>
+        <p>{type}</p>
+        <p>{dimension}</p>
+      </div>
+
+      <h2>Residents</h2>
+      {residents.map(({ id, name, status, image }) => (
+        <div key={id}>
+          <Image src={image} width={200} height={200} />
+          <h2>{name}</h2>
+          <p>{status}</p>
+          <Link
+            href={`
+          /characters/${id}`}
+          >
+            <a>View information</a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export async function getStaticPaths() {
