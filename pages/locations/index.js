dfda0f622283f64efd6d55e2e972a94ad440275c
Angel Padrino <plumreiss@gmail.com>
2021-05-17 01:07:05 -0400

    Feat: locations page created, fetching every location

--- pages/locations/index.js
+++ pages/locations/index.js
@@ -1,3 +1,51 @@
-export default function Locations() {
-  return <h1>Desde locations</h1>;
+import Link from "next/link";
+
+export default function Locations({ locations }) {
+  return (
+    <div>
+      {locations.map(({ id, name, type, dimension }) => (
+        <div>
+          <h2>{name}</h2>
+          <p>{type}</p>
+          <p>{dimension}</p>
+          <Link href={`locations/${id}`}>
+            <a>View residents</a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const API = "https://rickandmortyapi.com/api";
+
+  const res = await fetch(`${API}/location`);
+  const data = await res.json();
+
+  const pagesLocation = Array.from(
+    { length: data.info.pages },
+    (v, i) => i + 1
+  );
+
+  const locationsRes = await Promise.all(
+    pagesLocation.map((page) => {
+      const locationRes = fetch(`${API}/location/?page=${page}`);
+      return locationRes;
+    })
+  );
+
+  const locationsPages = await Promise.all(
+    locationsRes.map((location) => location.json())
+  );
+
+  const locations = locationsPages
+    .map((locationPage) => locationPage.results.map((location) => location))
+    .flat();
+
+  return {
+    props: {
+      locations,
+    },
+  };
 }
