059d4e155b44dc1092f179e3d2c2ec9c52e0b22f
Angel Padrino <plumreiss@gmail.com>
2021-05-18 19:05:44 -0400

    ADD: adding getStaticProp, fetching episodes and returning episodes props

--- pages/episodes/index.js
+++ pages/episodes/index.js
@@ -1,3 +1,33 @@
+const API = "https://rickandmortyapi.com/api";
+
 export default function Episodes() {
   return <h1>Desde episodes</h1>;
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API}/episode`);
+  const data = await res.json();
+
+  const episodesPages = Array.from(
+    { length: data.info.pages },
+    (v, i) => i + 1
+  );
+
+  const episodesRes = await Promise.all(
+    episodesPages.map((page) => fetch(`${API}/episode/?page=${page}`))
+  );
+
+  const episodesJson = await Promise.all(
+    episodesRes.map((episodeRes) => episodeRes.json())
+  );
+
+  const episodes = episodesJson.map((el) => el.results).flat();
+
+  console.log(episodes);
+
+  return {
+    props: {
+      episodes,
+    },
+  };
 }
