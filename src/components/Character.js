e5e4ec7fa43e1e2a51da30d34d626585425e858b
Angel Padrino <plumreiss@gmail.com>
2021-06-13 00:18:26 -0400

    refactor character to component

---
+++ src/components/Character.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function Character({
+  id,
+  name,
+  status,
+  species,
+  image,
+  locationName,
+  locationId,
+  episodeId,
+  episodeName,
+}) {
+  return (
+    <article>
+      <div>
+        <Image src={image} width={200} height={200} />
+      </div>
+      <div>
+        <div>
+          <Link href={`/characters/${id}`}>
+            <a>
+              <h2>{name}</h2>
+            </a>
+          </Link>
+          <span>
+            {status} - {species}
+          </span>
+        </div>
+        <div>
+          <span>Last known location:</span>
+          <Link href={`/locations/${locationId}`}>
+            <a>{locationName}</a>
+          </Link>
+        </div>
+        <div>
+          <span>First seen in:</span>
+          <Link href={`/episodes/${episodeId}`}>
+            <a>{episodeName}</a>
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+}
