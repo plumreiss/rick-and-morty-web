f001c55370155b457fbfa268c2e42145ba13a71b
Angel Padrino <plumreiss@gmail.com>
2021-08-01 23:46:24 -0400

    feat: add font family roboto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { Layout } from "../components/Layout";
 import { GlobalStyles } from "../styles/GlobalStyles";
+import "../styles/GlobalFonts.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
