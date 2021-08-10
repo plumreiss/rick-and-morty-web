7d75c041097011cefb6c3e9285eb5ce468f7d31a
Angel Padrino <plumreiss@gmail.com>
2021-08-09 21:43:47 -0400

    feat: style EpisodeCard component

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,14 +1,57 @@
 import Link from "next/link";
+import styled from "styled-components";
+
+const CardEpisode = styled.article`
+  display: flex;
+  width: 220px;
+  height: 300px;
+  flex-direction: column;
+  justify-content: space-evenly;
+  background-color: rgb(60, 62, 68, 1);
+  text-align: center;
+  border-radius: 0.7rem;
+`;
+
+const ContainerInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    color: rgb(158, 158, 158);
+  }
+
+  h3 {
+    color: #f4f4f4;
+  }
+`;
+
+const Title = styled.a`
+  color: #f4f4f4;
+  cursor: pointer;
+  font-size: 1.5rem;
+  text-decoration: underline;
+
+  &:hover {
+    color: orange;
+  }
+`;
 
 export function EpisodeCard({ id, name, air_date, episode }) {
   return (
-    <article>
-      <h2>{name}</h2>
-      <span>{air_date}</span>
-      <h3>{episode}</h3>
-      <Link href={`/episodes/${id}`}>
-        <a>View characters</a>
-      </Link>
-    </article>
+    <CardEpisode>
+      <ContainerInfo>
+        <Link href={`/episodes/${id}`}>
+          <Title>{name}</Title>
+        </Link>
+      </ContainerInfo>
+      <ContainerInfo>
+        <span>Air Date: </span>
+        <h3>{air_date}</h3>
+      </ContainerInfo>
+      <ContainerInfo>
+        <span>Season and Episode: </span>
+        <h3>{episode}</h3>
+      </ContainerInfo>
+    </CardEpisode>
   );
 }
