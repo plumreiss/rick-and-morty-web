e8a16e6cd39c8730dbdbd329d80cc91bc2e5ac79
Angel Padrino <plumreiss@gmail.com>
2021-08-14 19:57:01 -0400

    feat: style Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+
+const FormContainer = styled.form`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
 export function Form({ handleSubmit, children }) {
-  return <form onSubmit={handleSubmit}>{children}</form>;
+  return <FormContainer onSubmit={handleSubmit}>{children}</FormContainer>;
 }
