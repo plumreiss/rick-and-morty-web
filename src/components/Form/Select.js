3bd6c546068bf58cf6b72d1a701c36c18f1661f1
Angel Padrino <plumreiss@gmail.com>
2021-08-17 15:13:23 -0400

    WIP

--- src/components/Form/Select.js
+++ src/components/Form/Select.js
@@ -1,11 +1,21 @@
+import styled from "styled-components";
+
+const InputSelect = styled.select`
+  width: 100%;
+  padding: 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: rgb(51, 51, 51);
+`;
+
 export function Select({ selectName, handleChange, defaultValue, children }) {
   return (
-    <select
+    <InputSelect
       name={selectName}
       onChange={handleChange}
       defaultValue={defaultValue}
     >
       {children}
-    </select>
+    </InputSelect>
   );
 }
