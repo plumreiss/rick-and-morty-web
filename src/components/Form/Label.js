433e0ab1e086d5174541165f7258216cd67ec1c2
Angel Padrino <plumreiss@gmail.com>
2021-08-18 14:43:22 -0400

    add margin-right to label

--- src/components/Form/Label.js
+++ src/components/Form/Label.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const InputLabel = styled.label`
   color: rgb(51, 51, 51);
   font-size: 1.2rem;
+  font-weight: 600;
+  margin-right: 15px;
 `;
 
 export function Label({ labelName }) {
