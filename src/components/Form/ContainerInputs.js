8de4862900eef0a9d9bc1bb78c10f4bacacd394c
Angel Padrino <plumreiss@gmail.com>
2021-08-20 20:31:45 -0400

    feat: Inputs and label into column

--- src/components/Form/ContainerInputs.js
+++ src/components/Form/ContainerInputs.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const ContainerInput = styled.div`
   display: flex;
+  width: 100%;
+  flex-direction: column;
 `;
 
 export function ContainerInputs({ children }) {
